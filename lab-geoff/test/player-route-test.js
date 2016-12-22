d4fe3f01486a59c0fb038daa09773e49797834ba
Geoff Simons <geoff.simons@gmail.com>
2016-12-21 18:49:21 -0800

    Server start and create test

---
+++ lab-geoff/test/player-route-test.js
@@ -0,0 +1,42 @@
+'use strict';
+
+const expect = require('chai').expect;
+const request = require('superagent');
+const Player = require('../model/player.js');
+
+const server = require('../server.js');
+
+const url = `http://localhost:${server.PORT}/api/player`;
+
+const examplePlayer = {
+  name: 'Joe Player',
+  email: 'joe@foo.bar'
+};
+
+describe('Player Routes', function() {
+  before( done => {
+    server.start()
+    .then(done)
+    .catch(done);
+  });
+
+  describe('POST /api/player', function() {
+    describe('with a valid body', function() {
+      after( done => {
+        if(!this.tempPlayer) return done();
+        Player.remove({})
+        .then( () => done())
+        .catch(done);
+      });
+      it('should create a player', done => {
+        request.post(url)
+        .send(examplePlayer)
+        .end( (err, res) => {
+          expect(res.status).to.equal(201);
+          this.tempPlayer = res.body;
+          done();
+        });
+      });
+    }); //valid body
+  }); // POST /api/player
+});
